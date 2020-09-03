cb163a4f7801b2d4252ece503bd0af094b40eee7
vignesh-ragavan <vigneshr.vtalent@gmail.com>
2020-09-02 23:03:37 -0700

    added routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import { json } from 'body-parser';
+import {currentUserRouter} from './routes/current-user';
+import {signinRouter} from './routes/signin';
+import {signoutRouter} from './routes/signout';
+import {signup} from './routes/signup';
 
 const app = express();
 app.use(json());
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
   res.send('Hi there!');
